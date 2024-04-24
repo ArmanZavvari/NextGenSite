a41b095eaa962c90df9810887337ec46755b801d
Arman Zavvari <armanzavvari@gmail.com>
2024-04-24 16:17:40 +0330

    Add modal for 'Not Available' message

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,32 @@
-let menu = document.querySelector(".menu")
-let menuBtn = document.querySelector(".header__icon")
-let menuBtnIcon = document.querySelector(".header__icon i")
+let menu = document.querySelector(".menu");
+let menuBtn = document.querySelector(".header__icon");
+let menuBtnIcon = document.querySelector(".header__icon i");
 
-menuBtn.addEventListener("click",function (){
-    if (menuBtnIcon.classList.contains("fa-bars")){
-        menu.style.left = "0"
-        menuBtnIcon.classList = "fa fa-times"
-    } else {
-        menu.style.left = "-288px"
-        menuBtnIcon.classList = "fa fa-bars"
-    }
-})
+menuBtn.addEventListener("click", function () {
+  if (menuBtnIcon.classList.contains("fa-bars")) {
+    menu.style.left = "0";
+    menuBtnIcon.classList = "fa fa-times";
+  } else {
+    menu.style.left = "-288px";
+    menuBtnIcon.classList = "fa fa-bars";
+  }
+});
+
+// modal
+let modal = document.getElementById("myModal");
+let btn = document.getElementById("myBtn");
+let span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
